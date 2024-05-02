864d8796adcb7895dc36ae2519d376264e3cacbf
Amir Mansoor <qazimansor89@gmail.com>
2024-05-02 10:51:37 +0500

    add UI for recent blogs

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -3,46 +3,75 @@ import axios from "axios";
 import BlogCard from "../components/BlogCard";
 import TextField from "@mui/material/TextField";
 
-import { Button, Paper, Container, Typography } from "@mui/material";
+import { Button, Paper, Container, Typography, Stack } from "@mui/material";
 import { Box, Grid } from "@mui/material";
 
 import { Pagination } from "@mui/material";
-import EmptyScreen from "../components/common/EmptyScreen"
+import EmptyScreen from "../components/common/EmptyScreen";
 import BasicPagination from "../components/common/BasicPagination";
-import {PAGE_CONSTANT} from "../constants/common/all.constants"
+import { PAGE_CONSTANT } from "../constants/common/all.constants";
+
+const RecentBlogs = ({ recentBlogs }) => {
+  return (
+    <div>
+      <Typography variant="h6" sx={{ marginBottom: "1rem" }}>
+        Recent Blogs
+      </Typography>
+      {recentBlogs &&
+        recentBlogs.map((blog) => (
+          <Box key={blog._id} sx={{ marginBottom: "1rem" }}>
+            <BlogCard
+              id={blog._id}
+              title={blog.title}
+              content={blog.content}
+              image={blog.image_url}
+              video={blog.video_url}
+              name={blog.author.name}
+              email={blog.author.email}
+              likeCount={blog.likeCount}
+              createdAt={blog.createdAt}
+            />
+          </Box>
+        ))}
+    </div>
+  );
+};
 
 const Blog = () => {
   const [blogs, setBlogs] = useState([]);
   const [searchValue, setSearchValue] = useState("");
   const [pageNumber, setPageNumber] = useState(1);
-  const [totalPageNumber,setTotalPageNumber] = useState(null);
+  const [totalPageNumber, setTotalPageNumber] = useState(null);
 
   const getAllBlogs = async (searchValue) => {
-   
     try {
       if (searchValue) {
-        const {data} = await axios.get(
-          `${process.env.REACT_APP_ENVIRONMENT === "development"
-            ? `${process.env.REACT_APP_DEV_URL}/blog?search=${searchValue}&page=${pageNumber}&limit=${PAGE_CONSTANT.LIMIT_RESULT}`
-            : `${process.env.REACT_APP_PROD_URL}/blog?search=${searchValue}&page=${pageNumber}&limit=${PAGE_CONSTANT.LIMIT_RESULT}`
+        const { data } = await axios.get(
+          `${
+            process.env.REACT_APP_ENVIRONMENT === "development"
+              ? `${process.env.REACT_APP_DEV_URL}/blog?search=${searchValue}&page=${pageNumber}&limit=${PAGE_CONSTANT.LIMIT_RESULT}`
+              : `${process.env.REACT_APP_PROD_URL}/blog?search=${searchValue}&page=${pageNumber}&limit=${PAGE_CONSTANT.LIMIT_RESULT}`
           }
           `
         );
-        console.log("data", data);
+
         if (data?.data?.data) {
           setBlogs(data?.data?.data);
-          setTotalPageNumber(data?.data?.totalPages)
+
+          setTotalPageNumber(data?.data?.totalPages);
         }
       } else {
-        const {data} = await axios.get(
-          `${process.env.REACT_APP_ENVIRONMENT === "development"
-            ? `${process.env.REACT_APP_DEV_URL}/blog?page=${pageNumber}&limit=${PAGE_CONSTANT.LIMIT_RESULT}`
-            : `${process.env.REACT_APP_PROD_URL}/blog?page=${pageNumber}&limit=${PAGE_CONSTANT.LIMIT_RESULT}`
+        const { data } = await axios.get(
+          `${
+            process.env.REACT_APP_ENVIRONMENT === "development"
+              ? `${process.env.REACT_APP_DEV_URL}/blog?page=${pageNumber}&limit=${PAGE_CONSTANT.LIMIT_RESULT}`
+              : `${process.env.REACT_APP_PROD_URL}/blog?page=${pageNumber}&limit=${PAGE_CONSTANT.LIMIT_RESULT}`
           }`
         );
         if (data.data?.data) {
           setBlogs(data?.data?.data);
-          setTotalPageNumber(data?.data?.totalPages)
+
+          setTotalPageNumber(data?.data?.totalPages);
         }
       }
     } catch (error) {
@@ -67,10 +96,14 @@ const Blog = () => {
 
   return (
     <>
-
       <Container>
-
-        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }} >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
           <TextField
             fullWidth
             label="Search Blog By title,email,name"
@@ -80,24 +113,28 @@ const Blog = () => {
               display: "flex",
               alignItems: "center",
               marginTop: "40px",
-              marginBottom:"20px"
+              marginBottom: "20px",
             }}
             id="fullWidth"
-            onChange={(e) => {
-              // Do something with the search query here
-              changeHandler(e);
-            }}
+            onChange={changeHandler}
           />
-          <Button variant="outlined" onClick={searchHandler}>Search</Button>
+          <Button variant="outlined" onClick={searchHandler}>
+            Search
+          </Button>
         </Box>
 
-        <Grid container spacing={4} mt={4}>
-
-
-
-          {blogs &&
-            blogs.map((blog) => (
-              <Grid item xs={12} md={4}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginTop: "40px",
+          }}
+        >
+          {/* Main Blog List */}
+          <Box sx={{ flex: 2, margin: "10px", marginLeft: "10px" }}>
+            {blogs.map((blog) => (
+              <Box key={blog._id} sx={{ marginBottom: "1rem" }}>
                 <BlogCard
                   id={blog._id}
                   title={blog.title}
@@ -109,26 +146,33 @@ const Blog = () => {
                   likeCount={blog.likeCount}
                   createdAt={blog.createdAt}
                 />
-              </Grid>
+              </Box>
             ))}
+            {blogs.length === 0 && <EmptyScreen />}
+          </Box>
 
-          {blogs.length == 0 && (<EmptyScreen />)}
-
-        </Grid>
-
-        <Box sx={{
-          display: 'flex',
-          alignItems:"center",
-          justifyContent:"center",
-          marginTop:"20px"
-        }}>
-
-        {totalPageNumber != null && <BasicPagination onChange={handlePageChange}  page={totalPageNumber}/> } 
-            
+          {/* Recent Blogs */}
+          <Box sx={{ flex: 1 }}>
+            <RecentBlogs recentBlogs={blogs.slice(0, 2)} />
+          </Box>
         </Box>
-        
-      </Container>
 
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "20px",
+          }}
+        >
+          {totalPageNumber != null && (
+            <BasicPagination
+              onChange={handlePageChange}
+              page={totalPageNumber}
+            />
+          )}
+        </Box>
+      </Container>
     </>
   );
 };
