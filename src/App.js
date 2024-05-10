c1a50ab8e771656003a615b5f7415f39712bf8fa
techy1999 <116334237+techy1999@users.noreply.github.com>
2024-05-11 00:14:56 +0530

    Revert "app.js and routing system refacored"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import { routeConfig } from "./routeConfig";
-import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Blog from "./pages/Blog";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MyBlog from "./pages/MyBlog";
+import CreateBlog from "./pages/CreateBlog";
 import Footer from "./components/Footer";
+import BlogDetail from "./pages/BlogDetail";
+import Profile from "./pages/Profile";
+import NotFound from "./components/common/NotFound"
 import "./App.css";
+import Home from "./pages/Home";
+import NotFoundImage from "./assets/undraw_page_not_found.svg"
 
 function App() {
   return (
-    <div className="app">
+    <>
       <Header />
       <Routes>
-         { routeConfig.map((route) => <Route path={route.path} element={route.element}/>) }
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blog />} />
+        <Route path="/blog/:id" element={<BlogDetail />} />
+        <Route path="/my-blogs" element={<MyBlog />} />
+        <Route path="/blog" element={<CreateBlog />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+         {/* Wildcard route for handling 404 errors */}
+         <Route path="*" element={<NotFound imageUrl={NotFoundImage} />} />
+         <Route path="/api" element={null} />
       </Routes>
       <Footer />
-    </div>
+    </>
   );
 }
 
