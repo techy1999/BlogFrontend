fb69445d760706f3269ee92c2a8d696608ae417d
sudhanshu <sudhanshuraj8917@gmail.com>
2024-05-17 00:53:39 +0530

    blog card ui improved

---
+++ src/pages/courses/courseCategory.jsx
@@ -0,0 +1,9 @@
+// The page for showing the category like 
+// C++
+// Python
+// Javascript
+// MERN
+// Database
+// MongodbDB
+// ....
+
---
+++ src/pages/courses/courseList.jsx
@@ -0,0 +1,3 @@
+// This is page for each category list of courses available in that particular category 
+// course category shows in left side to filter from top we have search to search using atlas search feature of mongobd
+// 
